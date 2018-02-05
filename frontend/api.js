e468612f124e465af07c25343315c0ac30c3e140
chandrashekar869 <shekard.chandra869@gmail.com>
2018-02-05 23:55:09 +0530

    Update api.js

--- frontend/api.js
+++ frontend/api.js
@@ -185,10 +185,10 @@ app.post('/users/deviceList', function(req, res) {
  //connection_callback.query("SELECT a.device_id,b.gas_level,b.gas_detector,b.gas_leak,b.low_gas,b.power_level,c.coordinates,log_time FROM user_device_list a,device_log_current b,devicelist c where  a.user_id='"+user_id+"' and a.device_id = b.device_Id and a.device_id = c.device_id", function (err, result, fields) {
     connection_callback.query("SELECT distinct a.device_id,b.gas_level,b.gas_detector,b.gas_leak,b.low_gas,b.power_level,c.coordinates,log_time,d.ang2_threshold,d.ang2_lower_limit,d.ang3_threshold,d.ang3_lower_limit,e.http_post_interval FROM user_device_list a,device_log_current b,devicelist c left join analog d on c.device_id=d.device_id left join slave_config e on c.device_id=e.device_id  where  a.user_id='"+user_id+"' and a.device_id = b.device_Id and a.device_id = c.device_id", function (err, result, fields) {       
       if (err) throw err;
-      results.map(function(value,i){
+      result.map(function(value,i){
         connection_callback.query(" SELECT * from session_log where device_id='"+value.device_id+"' ORDER BY _id DESC LIMIT 1", function (err, result_id, fields){
-                results[i]["server_log_time"]=result_id[0].log_time;
-         if(i==results.length-1)
+                result[i]["server_log_time"]=result_id[0].log_time;
+         if(i==result.length-1)
             res.send(result);         
             });                   
         });
