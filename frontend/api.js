698801722e4e84bb3a8c73b6a1fe6c47bf84cf22
vinay <vinay.prithiani@gmail.com>
2018-09-10 15:42:12 +0530

    Original Source buildv1

--- frontend/api.js
+++ frontend/api.js
@@ -186,7 +186,19 @@ app.post('/users/deviceList', function(req, res) {
     connection_callback.query("SELECT distinct a.device_id,b.gas_level,b.gas_detector,b.gas_leak,b.low_gas,b.power_level,c.coordinates,log_time,d.ang2_threshold,d.ang2_lower_limit,d.ang3_threshold,d.ang3_lower_limit,e.http_post_interval FROM user_device_list a,device_log_current b,devicelist c left join analog d on c.device_id=d.device_id left join slave_config e on c.device_id=e.device_id  where  a.user_id='"+user_id+"' and a.device_id = b.device_Id and a.device_id = c.device_id", function (err, result, fields) {       
  console.log("SELECT a.device_id ,b.gas_leak,b.low_gas,b.power_level,c.coordinates,log_time FROM user_device_list a,device_log_current b,devicelist c where  a.user_id='"+user_id+"' and a.device_id = b.device_Id and a.device_id = c.device_id");
       if (err) throw err;
-    res.send(result);
+        else{
+            if(result.length==0)
+            res.send(result);
+            else
+            result.map(function(device_id,index){
+                connection_callback.query(" SELECT * from session_log where device_id='"+device_id+"' ORDER BY _id DESC LIMIT 1", function (err, result_id, fields){
+                    result[index]["server_log_time"]=result_id[0].log_time;
+                    if(index==result.length-1)
+                     res.send(result);
+                 });
+            });
+        }
+      //res.send(result);
    }); 
 connection_callback.end();
 });
